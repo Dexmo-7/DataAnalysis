b1db6a96acb5f7288c4c3afbbfc8cb4ac72659ff
Mateusz Owsianka <mowsianka13@gmail.com>
2020-09-26 13:48:15 +0200

    Add vector zeros and variable number = 0.

--- num_to_py.py
+++ num_to_py.py
@@ -1,8 +1,18 @@
 import numpy as np
 
 data = np.random.randn(2, 3)
+# vector with zeros
+data_1 = np.zeros(3)
+number = 0
 
 new_data = data * data
+# matrix 2x3 multiple by vector 0 equal 0
+new_data1 = data * data_1
+# matrix 2x3 multiple by 0 equal 0
+new_data2 = data * number
 
 print(data)
+print(data_1)
 print(new_data)
+print(new_data1)
+print(new_data2)
